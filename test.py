2a16b9b8033d03e57e921e5edb4a8e8daeb20149
fanstein <fanfly88@163.com>
2017-05-01 15:44:20 +0800

    5.1

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python
 # -*- coding=UTF8 -*-
-import xml.dom.minidom
-from xml.dom.minidom import parse
-#http://www.runoob.com/python/python-xml.html
-DOMTree =parse('./http_sampler.xml')
-collection=DOMTree.documentElement
-if collection.hasAttribute('version'):
-    print collection.getAttribute('version')
+from xml.etree.ElementTree import Element
+
+import xml_parser
+
+# 1. 读取xml文件
+tree = xml_parser.read_xml("./test.xml")
+
+# 5. 修改节点文本
+# 定位节点
+text_nodes = xml_parser.get_node_by_keyvalue(xml_parser.find_nodes(tree, "processers/services/service/chain"), {"sequency": "chain3"})
+xml_parser.change_node_text(text_nodes, "new text")
+
+# 6. 输出到结果文件
+xml_parser.write_xml(tree, "out.xml")
